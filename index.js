494f745de570061236f08443d93701df50bc660d
Fluturecode <fluturecode@gmail.com>
2023-04-10 16:29:56 -0400

    modified index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const {
+import {
   Connection,
   PublicKey,
   clusterApiUrl,
   Keypair,
   LAMPORTS_PER_SOL
-} = require('@solana/web3.js')
+} from '@solana/web3.js'
 
 const wallet = new Keypair()
 
 const publicKey = new PublicKey(wallet._keypair.publicKey)
-const secretKey = wallet._keypair.secretKey
 
 const getWalletBalance = async() => {
   try {
@@ -24,7 +23,7 @@ const getWalletBalance = async() => {
 const airDropSol = async() => {
   try {
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
-    const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+    const fromAirDropSignature = await connection.requestAirdrop(publicKey, 3 * LAMPORTS_PER_SOL)
     await connection.confirmTransaction({ signature: fromAirDropSignature })
   } catch(err) {
     console.log(err)
